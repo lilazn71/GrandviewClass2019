e8891de258b6a59c4dacac9cdeb8e2f467ac84e4
Michael Nguyen <michael.nguyen@grandview.edu>
2019-09-23 23:30:53 -0500

    hola

--- lessonphp/signin.php
+++ lessonphp/signin.php
@@ -8,10 +8,9 @@
 </head>
 <body>
 
-
 <form action="signin.php" method="POST" autocomplete="on">
     
-    Name: <input type="text" name="name" required><br>
+    Name: <input type="text" name="name" required><br>  
    
     E-mail: <input type="email" name="email" required><br>
 
@@ -30,22 +29,21 @@
     Gender:  
     
     <input type="radio" name="gender" value="male" checked> Male
-    <input type="radio" name="gender" value="female"> Female<br>
-   
+    <input type="radio" name="gender" value="female"> Female <br>
+
     How are you feeling today?<br>
 
     <input type="checkbox" name="good" value="Good" checked> I feel good<br>
     <input type="checkbox" name="bad" value="Bad"> I feel bad<br>
     <input type="checkbox" name="soso" value="SoSo"> I feel ok<br>
    
-    <input type="submit"><input type="reset">
-
-
-    Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-Phone number is: <?php echo $_POST["phone"]; ?>
-You are in the field of <?php echo $_POST["Occupation"]; ?>
-You identify as <?php echo $_POST["gender"]; ?>
-You are feeling <?php echo $_POST["checkbox"]; ?> 
+    <input type="submit"><input type="reset"><br><br><br>
+   
+    
+<br>Welcome <?php echo $_POST["name"]; ?><br>
+<br>Your email address is: <?php echo $_POST["email"]; ?><br>
+<br>Your phone number is: <?php echo $_POST["phone"] ; ?><br>
+<br>You are in the field of <?php echo $_POST["Occupation"]; ?><br>
+<br>You identify as <?php echo $_POST["gender"]; ?><br>
 </body>
 </html>
